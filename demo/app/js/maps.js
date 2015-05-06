8f8487f5e4efd816bdc92a7d9d7459f922556535
cody1991 <476490767@qq.com>
2015-05-06 12:31:49 +0800

    add a app demo

---
+++ demo/app/js/maps.js
@@ -0,0 +1,22 @@
+
+
+window.google = window.google || {};
+google.maps = google.maps || {};
+(function() {
+  
+  function getScript(src) {
+    document.write('<' + 'script src="' + src + '"><' + '/script>');
+  }
+  
+  var modules = google.maps.modules = {};
+  google.maps.__gjsload__ = function(name, text) {
+    modules[name] = text;
+  };
+  
+  google.maps.Load = function(apiLoad) {
+    delete google.maps.Load;
+    apiLoad([0.009999999776482582,[[["https://mts0.googleapis.com/vt?lyrs=m@298000000\u0026src=api\u0026hl=zh-CN\u0026","https://mts1.googleapis.com/vt?lyrs=m@298000000\u0026src=api\u0026hl=zh-CN\u0026"],null,null,null,null,"m@298000000",["https://mts0.google.com/vt?lyrs=m@298000000\u0026src=api\u0026hl=zh-CN\u0026","https://mts1.google.com/vt?lyrs=m@298000000\u0026src=api\u0026hl=zh-CN\u0026"]],[["https://khms0.googleapis.com/kh?v=170\u0026hl=zh-CN\u0026","https://khms1.googleapis.com/kh?v=170\u0026hl=zh-CN\u0026"],null,null,null,1,"170",["https://khms0.google.com/kh?v=170\u0026hl=zh-CN\u0026","https://khms1.google.com/kh?v=170\u0026hl=zh-CN\u0026"]],[["https://mts0.googleapis.com/vt?lyrs=h@298000000\u0026src=api\u0026hl=zh-CN\u0026","https://mts1.googleapis.com/vt?lyrs=h@298000000\u0026src=api\u0026hl=zh-CN\u0026"],null,null,null,null,"h@298000000",["https://mts0.google.com/vt?lyrs=h@298000000\u0026src=api\u0026hl=zh-CN\u0026","https://mts1.google.com/vt?lyrs=h@298000000\u0026src=api\u0026hl=zh-CN\u0026"]],[["https://mts0.googleapis.com/vt?lyrs=t@132,r@298000000\u0026src=api\u0026hl=zh-CN\u0026","https://mts1.googleapis.com/vt?lyrs=t@132,r@298000000\u0026src=api\u0026hl=zh-CN\u0026"],null,null,null,null,"t@132,r@298000000",["https://mts0.google.com/vt?lyrs=t@132,r@298000000\u0026src=api\u0026hl=zh-CN\u0026","https://mts1.google.com/vt?lyrs=t@132,r@298000000\u0026src=api\u0026hl=zh-CN\u0026"]],null,null,[["https://cbks0.googleapis.com/cbk?","https://cbks1.googleapis.com/cbk?"]],[["https://khms0.googleapis.com/kh?v=85\u0026hl=zh-CN\u0026","https://khms1.googleapis.com/kh?v=85\u0026hl=zh-CN\u0026"],null,null,null,null,"85",["https://khms0.google.com/kh?v=85\u0026hl=zh-CN\u0026","https://khms1.google.com/kh?v=85\u0026hl=zh-CN\u0026"]],[["https://mts0.googleapis.com/mapslt?hl=zh-CN\u0026","https://mts1.googleapis.com/mapslt?hl=zh-CN\u0026"]],[["https://mts0.googleapis.com/mapslt/ft?hl=zh-CN\u0026","https://mts1.googleapis.com/mapslt/ft?hl=zh-CN\u0026"]],[["https://mts0.googleapis.com/vt?hl=zh-CN\u0026","https://mts1.googleapis.com/vt?hl=zh-CN\u0026"]],[["https://mts0.googleapis.com/mapslt/loom?hl=zh-CN\u0026","https://mts1.googleapis.com/mapslt/loom?hl=zh-CN\u0026"]],[["https://mts0.googleapis.com/mapslt?hl=zh-CN\u0026","https://mts1.googleapis.com/mapslt?hl=zh-CN\u0026"]],[["https://mts0.googleapis.com/mapslt/ft?hl=zh-CN\u0026","https://mts1.googleapis.com/mapslt/ft?hl=zh-CN\u0026"]],[["https://mts0.googleapis.com/mapslt/loom?hl=zh-CN\u0026","https://mts1.googleapis.com/mapslt/loom?hl=zh-CN\u0026"]]],["zh-CN","US",null,0,null,null,"https://maps.gstatic.com/mapfiles/","https://csi.gstatic.com","https://maps.googleapis.com","https://maps.googleapis.com",null,"https://maps.google.com","https://gg.google.com","https://maps.gstatic.com/maps-api-v3/api/images/","https://www.google.com/maps",0],["https://maps.gstatic.com/maps-api-v3/api/js/20/10/intl/zh_cn","3.20.10"],[572400079],1,null,null,null,null,null,"",null,null,1,"https://khms.googleapis.com/mz?v=170\u0026","AIzaSyASm3CwaK9qtcZEWYa-iQwHaGi3gcosAJc","https://earthbuilder.googleapis.com","https://earthbuilder.googleapis.com",null,"https://mts.googleapis.com/vt/icon",[["https://mts0.googleapis.com/vt","https://mts1.googleapis.com/vt"],["https://mts0.googleapis.com/vt","https://mts1.googleapis.com/vt"],null,null,null,null,null,null,null,null,null,null,["https://mts0.google.com/vt","https://mts1.google.com/vt"],"/maps/vt",298000000,132],2,500,[null,"https://g0.gstatic.com/landmark/tour","https://g0.gstatic.com/landmark/config","","https://www.google.com/maps/preview/log204","","https://static.panoramio.com.storage.googleapis.com/photos/",["https://geo0.ggpht.com/cbk","https://geo1.ggpht.com/cbk","https://geo2.ggpht.com/cbk","https://geo3.ggpht.com/cbk"]],["https://www.google.com/maps/api/js/master?pb=!1m2!1u20!2s10!2szh-CN!3sUS!4s20/10/intl/zh_cn","https://www.google.com/maps/api/js/widget?pb=!1m2!1u20!2s10!2szh-CN"],null,0,0,"/maps/api/js/ApplicationService.GetEntityDetails",0], loadScriptTime);
+  };
+  var loadScriptTime = (new Date).getTime();
+  getScript("https://maps.gstatic.com/maps-api-v3/api/js/20/10/intl/zh_cn/main.js");
+})();
