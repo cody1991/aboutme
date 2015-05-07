bc00f7885e3633ca54382542a6790bac9c58b117
cody1991 <476490767@qq.com>
2015-05-07 13:32:31 +0800

    add waypoints

--- demo/waypoints/js/common.js
+++ demo/waypoints/js/common.js
@@ -4,5 +4,20 @@ var waypoint = new Waypoint({
         alert('Basic waypoint triggered', direction, this.element.id + ' triggers at ' + this.triggerPoint);
     },
     offset: '40px',
-    context:document.getElementsByClassName('container')[0]
+    context: document.getElementsByClassName('container')[0]
+})
+
+
+var waypoint2 = new Waypoint({
+    element: document.getElementById('nav'),
+    handler: function(direction) {
+    	console.log(direction)
+        if (direction === 'down') {
+            this.element.style.position = 'fixed';
+            this.element.style.top = '0px'
+        }
+        else{
+        	this.element.style.position = 'static';
+        }
+    },
 })
