6e38b4836f458b8b3bdc8c9bfe9d6446380539b3
cody1991 <476490767@qq.com>
2015-05-07 19:09:34 +0800

    change

--- demo/waypoints/js/common.js
+++ demo/waypoints/js/common.js
@@ -1,7 +1,7 @@
 var waypoint = new Waypoint({
     element: document.getElementById('waypoint'),
     handler: function(direction) {
-        alert('Basic waypoint triggered', direction, this.element.id + ' triggers at ' + this.triggerPoint);
+        console.log('Basic waypoint triggered', direction, this.element.id + ' triggers at ' + this.triggerPoint);
     },
     offset: '40px',
     context: document.getElementsByClassName('container')[0]
@@ -11,7 +11,7 @@ var waypoint = new Waypoint({
 var waypoint2 = new Waypoint({
     element: document.getElementById('nav'),
     handler: function(direction) {
-    	console.log(direction)
+    	console.log(direction , this.group.name , this.options ,this.triggerPoint);
         if (direction === 'down') {
             this.element.style.position = 'fixed';
             this.element.style.top = '0px'
