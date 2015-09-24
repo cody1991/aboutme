f5900cd391887cd3db6af8089bcda20c4814501f
cody1991 <476490767@qq.com>
2015-09-24 15:51:46 +0800

    remove demo folder

--- demo/react-0.13.0/src/helloworld.js
+++
@@ -1,1 +0,0 @@
-React.render( < h1 > Hello, world! < /h1>, document.getElementById('example') );
--- demo/skrollr/js/common.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    var s = skrollr.init({
-        smoothScrolling: false,
-        mobileDeceleration: 0.004
-    });
-})
